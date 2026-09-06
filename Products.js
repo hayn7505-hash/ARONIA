514db34cb1c3e443dfda7b44361943c3a0978fad
hayn7505-hash <hayn7505@gmail.com>
2026-09-06 13:56:49 +0300

    Update Products.js

--- Products.js
+++ Products.js
@@ -1,1 +1,34 @@
-
+const products = [
+  {
+    id: 1,
+    name: "Hydrating Serum",
+    nameAr: "سيروم ترطيب البشرة",
+    category: "العناية بالبشرة",
+    price: 25000,
+    image: "https://placehold.co/600x600/f8eee9/8a7777?text=Serum"
+  },
+  {
+    id: 2,
+    name: "Gentle Cleanser",
+    nameAr: "غسول لطيف للوجه",
+    category: "العناية بالبشرة",
+    price: 18000,
+    image: "https://placehold.co/600x600/f8eee9/8a7777?text=Cleanser"
+  },
+  {
+    id: 3,
+    name: "Beauty Cream",
+    nameAr: "كريم العناية اليومية",
+    category: "العناية بالبشرة",
+    price: 22000,
+    image: "https://placehold.co/600x600/f8eee9/8a7777?text=Cream"
+  },
+  {
+    id: 4,
+    name: "Beauty Essentials",
+    nameAr: "مستلزمات الجمال",
+    category: "المكياج",
+    price: 30000,
+    image: "https://placehold.co/600x600/f8eee9/8a7777?text=Beauty"
+  }
+];
